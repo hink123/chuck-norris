b919e53c478bc13ea8a14a7664ea54929e82087f
Will Hinkelman <will.hink24@gmail.com>
2019-11-20 15:47:03 -0800

    displays joke

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Chuck', joke: null });
 });
+
+router.post('/', function(req, res) {
+  request('https://api.chucknorris.io/jokes/random',
+  function(err, response, body) {
+    var joke = JSON.parse(body);
+    console.log('joke: ' + body)
+    res.render('index', {joke: joke});
+  })
+})
 
 module.exports = router;
